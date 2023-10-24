05e0878807cd5cb2ca4fd116c7fb3e84d1afb834
James Hines <jameshines10@gmail.com>
2023-10-24 00:21:38 +0000

    Updates the arbiter address

--- migrations/1_deploy_contracts.js
+++ migrations/1_deploy_contracts.js
@@ -1,5 +1,5 @@
 const RPSContract = artifacts.require('RPSContract');
 
 module.exports = function (deployer) {
-  deployer.deploy(RPSContract, 1350, '0x5361b326d932dA0885b97AAb86B659d099bdb7f2');
+  deployer.deploy(RPSContract, 1350, '0x3b10f9d3773172f2f74bB1Bb8EfBCF18626b3bE8');
 };
